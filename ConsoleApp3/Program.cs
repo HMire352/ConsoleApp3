25d8d341e59ef921e8f0f101e7739409c1a7bf0f
Pingvu <nhlpool27@gmail.com>
2023-01-20 14:31:31 +0100

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace inlämmning
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv in 5 namn");
+            string[] name = new string[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                name[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("namnerna är: ");
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine(name[i]);
+
+
+
+        }
+    }
+}
